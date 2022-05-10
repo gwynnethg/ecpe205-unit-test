fcd32af5eab1337425fc4a3838d715f4d5e60302
gwynneth.g <s2020018@usls.edu.ph>
2022-05-10 22:30:15 +0800

    calculatortest

--- src/com/ecpe205/Calculator.java
+++ src/com/ecpe205/Calculator.java
@@ -1,26 +1,60 @@
 package com.ecpe205;
 
+import java.util.Arrays;
+
 public class Calculator {
-    public double sum( double a, double b ) {
+    public double sum(double a, double b) {
         return a + b;
     }
-    public boolean isEven (int value) {
+
+    public boolean isEven(int value) {
         return value % 2 == 0;
     }
-    public boolean isOdd (int value) {
+
+    public boolean isOdd(int value) {
         return value % 2 == 1;
     }
 
     //create a method that computes base x power of y
     //use parameterizedTest methodsource - 5 sets to test
+    public double exponent(double x, double y) {
+        return Math.pow(x, y);
+    }
 
     //create a method that computes for the factorial of a value
     //use parameterizedTest- 5 values to test
+    public long factorial(int n) {
+        return (1 > n) ? 1 : n * factorial(n - 1);
+    }
 
     //create a method that checks if a given string is a palindrome
     //use parameterizedTest - 5 values to test
+    static boolean isPalindrome(String str) {
+        String OriginalString = str;
+        int i = 0, j = str.length() - 1;
+        while (i < j) {
+            if (str.charAt(i) != str.charAt(j)) return false;
+            i++;
+            j--;
+        }
+        return OriginalString.equalsIgnoreCase(str);
+    }
 
     //create a method that accepts an array of integer values, the method then sorts the value in ascending order
     //** you may choose whatever sort data structure (bubble sort, shell sort,...)
     //use parameterizedTest methodSource
+    static String bubbleSort(int[] array) {
+        int size = array.length;
+        for (int i = 0; i < size - 1; i++)
+            for (int j = 0; j < size - i - 1; j++)
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+        return Arrays.toString(array);
+    }
+// Repository Link: https://github.com/gwynnethg/CalculatorActivity.git
+    //Gwynneth B. Gonzaga
 }
+
--- src/com/ecpe205/CalculatorTest.java
+++ src/com/ecpe205/CalculatorTest.java
@@ -13,21 +13,21 @@ class CalculatorTest {
     Calculator calc;
 
     @BeforeAll
-    void setup(){
+    void setup() {
         calc = new Calculator();
     }
 
     //CREATE A TEST TO TEST isEven
     @Test
     void shouldCheckValueIsEven() {
-        assertEquals(true,calc.isEven(2));
-        assertEquals(false,calc.isEven(1));
+        assertEquals(true, calc.isEven(2));
+        assertEquals(false, calc.isEven(1));
     }
 
     @Test
     void shouldCheckValueIsOdd() {
-        assertEquals(true,calc.isOdd(1));
-        assertEquals(false,calc.isOdd(2));
+        assertEquals(true, calc.isOdd(1));
+        assertEquals(false, calc.isOdd(2));
     }
 
 
@@ -36,15 +36,15 @@ class CalculatorTest {
     void shouldSumTwoEncodedValues() {
 
         // 1 + 2 = 3
-        assertEquals(3, calc.sum(1,2) );
-        assertEquals(9, calc.sum(5,4) );
-        assertEquals(5, calc.sum(3,2) );
-        assertEquals(14, calc.sum(5,9) );
+        assertEquals(3, calc.sum(1, 2));
+        assertEquals(9, calc.sum(5, 4));
+        assertEquals(5, calc.sum(3, 2));
+        assertEquals(14, calc.sum(5, 9));
     }
 
     @ParameterizedTest
-    @ValueSource(ints = {1,2,3,4,5})
-    void shouldSumValueWithOne( int value ) {
+    @ValueSource(ints = {1, 2, 3, 4, 5})
+    void shouldSumValueWithOne(int value) {
         assertEquals(value + 1, calc.sum(value, 1));
     }
 
@@ -54,21 +54,70 @@ class CalculatorTest {
         assertEquals(a + b, calc.sum(a, b));
     }
 
-    static Stream<Arguments> sumInputValues () {
+    static Stream<Arguments> sumInputValues() {
         return Stream.of(
-                Arguments.of(1,2),
-                Arguments.of(4,6),
-                Arguments.of(2,7),
-                Arguments.of(3,7),
-                Arguments.of(3,0)
+                Arguments.of(1, 2),
+                Arguments.of(4, 6),
+                Arguments.of(2, 7),
+                Arguments.of(3, 7),
+                Arguments.of(3, 0)
         );
     }
 
-    static Stream<Arguments> arrayOfIntegerSets () {
+    static Stream<Arguments> arrayOfIntegerSets() {
         return Stream.of(
-                Arguments.of(new int[]{1,2,3,4,1}),
-                Arguments.of(new int[]{7,1,5,4,1}),
-                Arguments.of(new int[]{7,2,8,3,4})
+                Arguments.of(new int[]{1, 2, 3, 4, 1}),
+                Arguments.of(new int[]{7, 1, 5, 4, 1}),
+                Arguments.of(new int[]{7, 2, 8, 3, 4})
+        );
+    }
+
+    //Test for Exponent
+    @ParameterizedTest
+    @MethodSource("useBaseXPowerY")
+    void shouldSolveExponent(double a, double b) {
+        assertEquals(Math.pow(a, b), calc.exponent(a, b));
+    }
+
+    static Stream<Arguments> useBaseXPowerY() {
+        return Stream.of(
+                Arguments.of(5, 2),
+                Arguments.of(6, 4),
+                Arguments.of(2, 3),
+                Arguments.of(9, 2),
+                Arguments.of(1, 0)
+        );
+    }
+
+    //Test for Factorial
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3, 4, 5})
+    void shouldSolveFactorial(int n) {
+        assertEquals(6, calc.factorial(n));
+    }
+
+    //Test for Palindrome
+    @ParameterizedTest
+    @ValueSource(strings = {"civic", "level", "valorant", "madam", "repaper"})
+    void shouldKnowIfStringIsPalindrome(String n) {
+        assertEquals(true, calc.isPalindrome(n));
+    }
+
+    //Test for Sorting
+    @ParameterizedTest
+    @MethodSource("arrayOfInteger")
+    void shouldSortArray(int[] array) {
+        assertEquals(calc.bubbleSort(array));
+    }
+
+    static Stream<Arguments> arrayOfInteger () {
+        return Stream.of(
+                Arguments.of(new int[]{4, 1, 3, 5, 2}),
+                Arguments.of(new int[]{22, 1, 45, 23, 15}),
+                Arguments.of(new int[]{6, 8, 9, 7, 10}),
+                Arguments.of(new int[]{55, 22, 44, 77, 88}),
+                Arguments.of(new int[]{2, 1, 15, 11, 3})
+
         );
     }
 }
